dbcd935bf479b961693a413ae7acd1f7e6948865
Max Chuquimia <maxchuquimia@noreply.users.github.com>
2019-08-02 14:39:18 +1000

    Add formula for git-pr

---
+++ Formula/git-pr.rb
@@ -0,0 +1,12 @@
+class GitPr < Formula
+  desc "Adds a 'pr' command to Git that opens the first output URL in your web browser"
+  homepage ""
+  url "https://raw.githubusercontent.com/maxchuquimia/homebrew-scripts/master/releases/git-pr-1.0.0.gz"
+  sha256 "958d41e7838a26ea37078d6d79f58a68aeac7bbad3d2026fcae666bb5672ea94"
+
+  bottle :unneeded
+
+  def install
+    bin.install "git-pr"
+  end
+end
