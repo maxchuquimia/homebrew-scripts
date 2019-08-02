c94020cb6fc7c5b0a33f25e52e2d931163e1c7ae
Max Chuquimia <maxchuquimia@noreply.users.github.com>
2019-08-02 14:45:05 +1000

    Updated git-pr formula

--- Formula/git-pr.rb
+++ Formula/git-pr.rb
@@ -3,6 +3,9 @@ class GitPr < Formula
   homepage ""
   url "https://raw.githubusercontent.com/maxchuquimia/homebrew-scripts/master/releases/git-pr-1.0.0.gz"
   sha256 "958d41e7838a26ea37078d6d79f58a68aeac7bbad3d2026fcae666bb5672ea94"
+  version "1.0.0"
+
+  depends_on "git"
 
   bottle :unneeded
 
