ca593ad0e5def5b4dea6dafcd3b2a15028405040
Max Chuquimia <maxchuquimia@noreply.users.github.com>
2019-08-02 15:07:17 +1000

    Fix file name

--- Formula/git-pr.rb
+++ Formula/git-pr.rb
@@ -1,7 +1,7 @@
 class GitPr < Formula
   desc "Adds a 'pr' command to Git that opens the first output URL in your web browser"
   homepage ""
-  url "https://raw.githubusercontent.com/maxchuquimia/homebrew-scripts/master/releases/git-pr-1.0.0.gz"
+  url "https://raw.githubusercontent.com/maxchuquimia/homebrew-scripts/master/releases/git-pr-1.0.0.tar.gz"
   sha256 "958d41e7838a26ea37078d6d79f58a68aeac7bbad3d2026fcae666bb5672ea94"
   version "1.0.0"
 
