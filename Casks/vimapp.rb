d7975d90859d43e33362fe2d64649ed37a0d462b
Max Chuquimia <Max.Chuquimia@foxsports.com.au>
2019-10-24 12:44:34 +1100

    Added an app that opens files in Terminal VIM

---
+++ Casks/vimapp.rb
@@ -0,0 +1,12 @@
+cask "vimapp" do
+  desc "An AppleScript to allow opening files in Terminal windows"
+  homepage ""
+  url "https://raw.githubusercontent.com/maxchuquimia/scripts/master/releases/VIM-1.0.0.dmg"
+  sha256 "fbf4c88c6ae66755b7f45ff8f408733bcb8d0dc23211497cd009176468c887e2"
+  version "1.0.0"
+
+  name "VIM"
+
+  app "VIM.app"
+end
+
