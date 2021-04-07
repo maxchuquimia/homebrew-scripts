0360728ddc3e3fbcc36c79540097c81c116c85b0
Max Chuquimia <max.chuquimia@users.noreply.github.io>
2021-04-07 22:06:15 +1000

    Update xcgrapher

--- Formula/xcgrapher.rb
+++ Formula/xcgrapher.rb
@@ -2,15 +2,14 @@ class Xcgrapher < Formula
   desc "Framework-level dependency graph generator for Xcode projects "
   homepage "https://github.com/maxchuquimia/xcgrapher"
   url "https://github.com/maxchuquimia/xcgrapher.git",
-      :tag => "0.0.2", :revision => "21820f3fa0886ed44336966080a3670f306c34f8"
+      :tag => "0.0.5", :revision => "95392d1d06daff61abd896e772d711a2b0ddc813"
   head "https://github.com/maxchuquimia/xcgrapher.git"
 
   depends_on :xcode => ["12.4", :build]
   depends_on "graphviz"
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/xcgrapher"
+    system "make", "install"
     puts "[!]"
     puts "[!] You *may* still need to install 'xcodeproj': try 'gem install xcodeproj' or visit https://github.com/CocoaPods/Xcodeproj"
     puts "[!]"
