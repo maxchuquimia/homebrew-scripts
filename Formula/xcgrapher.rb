c67f2bf70244e7dcccc4677f2e0884d1d8c30153
Max Chuquimia <max.chuquimia@users.noreply.github.io>
2021-04-10 12:32:45 +1000

    Update xcgrapher

--- Formula/xcgrapher.rb
+++ Formula/xcgrapher.rb
@@ -2,7 +2,7 @@ class Xcgrapher < Formula
   desc "Framework-level dependency graph generator for Xcode projects "
   homepage "https://github.com/maxchuquimia/xcgrapher"
   url "https://github.com/maxchuquimia/xcgrapher.git",
-      :tag => "0.0.8", :revision => "e5ecf00c2d9326dac61ab7c0c278999310a1b32c"
+      :tag => "0.0.9", :revision => "d5b410f1f42bb95ce51453da539834d3678d85a4"
   head "https://github.com/maxchuquimia/xcgrapher.git"
 
   depends_on :xcode => ["12.4", :build]
