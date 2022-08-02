b69f83719531129b6ede6f60be83403a1db47230
Max Chuquimia <max.chuquimia@users.noreply.github.io>
2022-08-02 13:35:44 +1000

    Update dotprofile with working directory caching/restoration

--- Formula/dotprofile.rb
+++ Formula/dotprofile.rb
@@ -1,12 +1,12 @@
 class Dotprofile < Formula
   desc "My personal profile source"
   homepage ""
-  url "https://raw.githubusercontent.com/maxchuquimia/homebrew-scripts/master/releases/dotprofile-1.0.1.tar.gz"
-  sha256 "dd2dc29acf11f48db8778a465716a8265aa3c935e5b363e9876467581b0b6b5b"
-  version "1.0.1"
+  url "https://raw.githubusercontent.com/maxchuquimia/homebrew-scripts/master/releases/dotprofile-1.1.0.tar.gz"
+  sha256 "4cac874dcb9cd223c6fecb06f68b57fc92c821b4a517195489871687ae346f62"
+  version "1.1.0"
 
   def install
       bin.install "dotprofile"
-      puts "Now add 'source /usr/local/bin/dotprofile' to your .profile or .bash_profile"
+      puts "Now add 'source /whatever-your-brew-path-is/dotprofile' to your .profile or .bash_profile"
   end
 end
